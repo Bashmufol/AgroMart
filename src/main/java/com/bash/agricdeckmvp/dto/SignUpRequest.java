8db168379a0b0bbbbbdb4747bd707d0b65c6ea86
Muhammed Bashir <bashmufol@gmail.com>
2025-09-04 22:30:07 +0100

    updated

--- src/main/java/com/bash/agricdeckmvp/dto/SignUpRequest.java
+++ src/main/java/com/bash/agricdeckmvp/dto/SignUpRequest.java
@@ -3,20 +3,26 @@ package com.bash.agricdeckmvp.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.Data;
 
-public class SignUpRequest {
-    @Email(message = "Must be a valid email")
-    @NotBlank
-    private String email;
-    @NotBlank(message = "First name is required")
-    private String firstName;
-    @NotBlank(message = "Last name is required")
-    private String lastName;
-    @NotBlank(message = "Country is required")
-    private String country;
-    @NotBlank(message = "Phone number is required")
-    private String phoneNumber;
-    @Size(min = 8, message = "Password must be at least 8 characters long")
-    @NotBlank
-    private String password;
-}
+public record SignUpRequest(
+        @Email(message = "Must be a valid email")
+        @NotBlank
+        String email,
+
+        @NotBlank(message = "First name is required")
+        String firstName,
+
+        @NotBlank(message = "Last name is required")
+        String lastName,
+
+        @NotBlank(message = "Country is required")
+        String country,
+
+        @NotBlank(message = "Phone number is required")
+        String phoneNumber,
+
+        @Size(min = 8, message = "Password must be at least 8 characters long")
+        @NotBlank
+        String password
+) {}
